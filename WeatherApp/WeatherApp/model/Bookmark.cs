cfefbf49377d2a4fa2c6afe136ba74bca4a0eb54
BojanaZoric <bjnzrc@gmail.com>
2019-04-04 17:18:58 +0200

    History, bookmark

---
+++ WeatherApp/WeatherApp/model/Bookmark.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WeatherApp
+{
+	class Bookmark
+	{
+		public List<string> BookmarkItems { get; set; }
+
+		public Bookmark()
+		{
+			BookmarkItems = new List<string>();
+
+			using (StreamReader sr = new StreamReader("bookmark.txt"))
+			{
+				string item;
+				while ((item = sr.ReadLine()) != null)
+				{
+					BookmarkItems.Add(item);
+				}
+			}
+
+		}
+
+		public bool AddItem(string item)
+		{
+
+			BookmarkItems.Add(item);
+			WriteToFile();
+
+			return true;
+		}
+
+		public bool RemoveItem(string item)
+		{
+			BookmarkItems.Remove(item);
+			WriteToFile();
+			return true;
+		}
+
+		public bool EmptyBookmark()
+		{
+			BookmarkItems.Clear();
+			WriteToFile();
+			return true;
+		}
+
+		public void WriteToFile()
+		{
+			using (StreamWriter writer = new StreamWriter("bookmark.txt"))
+			{
+				foreach (string s in BookmarkItems)
+				{
+					writer.WriteLine(s);
+				}
+			}
+		}
+
+		public string MakeString()
+		{
+			string str = "";
+			foreach (string s in BookmarkItems)
+			{
+				str += s + " ";
+			}
+			return str;
+		}
+	}
+}
---
+++ WeatherApp/WeatherApp/model/History.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WeatherApp
+{
+	class History
+	{
+		public List<string> HistoryItems { get; set; }
+
+		public History()
+		{
+			HistoryItems = new List<string>();
+
+			using (StreamReader sr = new StreamReader("history.txt"))
+			{
+				string item;
+				while ((item = sr.ReadLine()) != null)
+				{
+					HistoryItems.Add(item);
+				}
+			}
+		}
+
+		public bool AddItem(string item)
+		{
+
+			HistoryItems.Add(item);
+			WriteToFile();
+
+			return true;
+		}
+
+		public bool RemoveItem(string item)
+		{
+			HistoryItems.Remove(item);
+			WriteToFile();
+			return true;
+		}
+
+		public bool EmptyHistory()
+		{
+			HistoryItems.Clear();
+			WriteToFile();
+			return true;
+		}
+
+		public void WriteToFile()
+		{
+			using (StreamWriter writer = new StreamWriter("history.txt"))
+			{
+				foreach (string s in HistoryItems)
+				{
+					writer.WriteLine(s);
+				}
+			}
+		}
+
+		public string MakeString()
+		{
+			string str = "";
+			foreach (string s in HistoryItems)
+			{
+				str += s + " ";
+			}
+			return str;
+		}
+
+	}
+}
